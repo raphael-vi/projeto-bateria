2f9963736261cc568cd49e9ff7b11626ed5832ef
raphael-vi <raphaelb.menezess@gmail.com>
2022-05-29 11:22:15 -0300

    Web Bateria
    Em js, Html e css

---
+++ script.js
@@ -0,0 +1,24 @@
+window.addEventListener('keydown', function(e){
+    
+    const key = document.querySelector(`.key[data-key="${e.key}"]`)
+    const audio = document.querySelector(`audio[data-key="${e.key}"]`)
+    
+    if (!audio) return;
+    
+    key.classList.add('playing');
+    
+
+    audio.currentTime= 0;
+    audio.play();
+
+})
+
+function removeTransition(e){
+    if(e.propertyName !== 'transform')return;
+    this.classList.remove('playing')
+
+    
+}
+
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener("transitionend", removeTransition))
